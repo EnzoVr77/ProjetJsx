e2ed532144d030e522261ddb7630f255d00470bb
De Sousa Alves Etann <etann.de-sousa-alves@etu.univ-orleans.fr>
2025-03-31 14:34:08 +0200

    :lipstick: Ajout NavBar Tailwind

--- projetapi/src/App.tsx
+++ projetapi/src/App.tsx
@@ -1,57 +1,19 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Import de React Router
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Home from './pages/Home';
 import Details from './pages/Details';
 import Albums from './pages/Albums.tsx';
 import Chanteurs from './pages/Chanteurs.tsx';
+import Navbar from './pages/Navbar.tsx';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <Router>
             <>
                 <div>
-                    <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-                        <img src={viteLogo} className="logo" alt="Vite logo" />
-                    </a>
-                    <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-                        <img src={reactLogo} className="logo react" alt="React logo" />
-                    </a>
+                    <Navbar />
                 </div>
-                <h1>Vite + React</h1>
-                <div className="card">
-                    <button onClick={() => setCount((count) => count + 1)}>
-                        count is {count}
-                    </button>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to test HMR
-                    </p>
-                </div>
-                <p className="read-the-docs">
-                    Click on the Vite and React logos to learn more
-                </p>
-
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Accueil</Link>
-                        </li>
-                        <li>
-                            <Link to="/details">Détails</Link>
-                        </li>
-                        <li>
-                            <Link to="/albums">Albums</Link>
-                        </li>
-                        <li>
-                            <Link to="/chanteurs">Chanteurs</Link>
-                        </li>
-                    </ul>
-                </nav>
 
                 <Routes>
                     <Route path="/" element={<Home />} />
